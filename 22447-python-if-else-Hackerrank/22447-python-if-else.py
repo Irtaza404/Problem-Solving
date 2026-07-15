861991b1ced6d05f3d77a017fde113058ee29a87
Muhammad Irtaza <90450152+Irtaza404@users.noreply.github.com>
2026-07-15 21:34:50 +0500

    Next

---
+++ 22447-python-if-else-Hackerrank/22447-python-if-else.py
@@ -0,0 +1,26 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    if n%2!=0:
+        print("Weird")
+    else:
+        if n in range(1,6):
+            print("Not Weird")
+        elif n in range(6,21):
+            print("Weird")
+        else:
+            print("Not Weird")
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
