de9e1d85ee3f6815679422d0ca3f65a022f5d519
Muhammad Irtaza <90450152+Irtaza404@users.noreply.github.com>
2026-07-17 21:49:42 +0500

    way to go

---
+++ 8026-print-function-Hackerrank/8026-print-function.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    n = int(input())
+    print("".join(map(str,range(1,n+1))))
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
