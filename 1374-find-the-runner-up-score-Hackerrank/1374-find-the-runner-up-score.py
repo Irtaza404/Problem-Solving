dc41a447365e196299f1158c5a90c7a5b70b6aa0
Muhammad Irtaza <90450152+Irtaza404@users.noreply.github.com>
2026-07-17 21:54:43 +0500

    way to go

---
+++ 1374-find-the-runner-up-score-Hackerrank/1374-find-the-runner-up-score.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    n = int(input())
+    arr = sorted(set(map(int, input().split())))[-2]
+    print(arr)
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
