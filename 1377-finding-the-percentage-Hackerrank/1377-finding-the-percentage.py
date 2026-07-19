c3d5cd21a59077b28c14cd4c895247ebd56964f9
Muhammad Irtaza <90450152+Irtaza404@users.noreply.github.com>
2026-07-19 15:13:08 +0500

    next

---
+++ 1377-finding-the-percentage-Hackerrank/1377-finding-the-percentage.py
@@ -0,0 +1,14 @@
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+    print(f"{sum(student_marks.get(query_name,[0]))/3:.2f}")
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
