cdbc0b5781b1c4dd4474e4eca344359dc30ea094
Muhammad Irtaza <90450152+Irtaza404@users.noreply.github.com>
2026-07-19 15:44:47 +0500

    next

---
+++ 7889-tuples-Hackerrank/7889-tuples.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    n = int(raw_input())
+    integer_list = map(int, raw_input().split())
+    print(hash(tuple(integer_list)))
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
