907cc46804cbff8b2194960da86a0c652d1d5f46
Muhammad Irtaza <90450152+Irtaza404@users.noreply.github.com>
2026-07-21 22:25:34 +0500

    next

---
+++ 8059-string-formatting-Hackerrank/8059-string-formatting.py
@@ -0,0 +1,11 @@
+def print_formatted(number):
+    # your code goes here
+    width = len(f"{n:b}")
+    for i in range(1,number+1):
+        print(f"{i:{width}} {i:{width}o} {i:{width}X} {i:{width}b}")
+
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
