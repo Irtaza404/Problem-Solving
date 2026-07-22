cc6a59a6b0c84c3cd09c66a6c331c048189e7361
Muhammad Irtaza <90450152+Irtaza404@users.noreply.github.com>
2026-07-22 23:10:09 +0500

    next

---
+++ 9375-introduction-to-sets-Hackerrank/9375-introduction-to-sets.py
@@ -0,0 +1,9 @@
+def average(array):
+    # your code goes here
+    return f"{sum(set(arr))/len(set(array)):.3f}"
+
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
