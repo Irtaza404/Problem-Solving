d4b868f7807cd23ca38620e010a33fe3cff9e823
Muhammad Irtaza <90450152+Irtaza404@users.noreply.github.com>
2026-07-23 23:28:50 +0500

    next

---
+++ 7887-mod-divmod-Hackerrank/7887-mod-divmod.py
@@ -0,0 +1,12 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+a=int(input())
+b=int(input())
+div=divmod(a,b)
+print(div[0])
+print(div[1])
+print(div)
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
