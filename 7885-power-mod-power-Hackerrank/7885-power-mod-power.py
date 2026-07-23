b376f71e480a85b97e811bffdacb7403c28d0035
Muhammad Irtaza <90450152+Irtaza404@users.noreply.github.com>
2026-07-23 23:35:30 +0500

    next

---
+++ 7885-power-mod-power-Hackerrank/7885-power-mod-power.py
@@ -0,0 +1,12 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import math
+a=int(input())
+b=int(input())
+m=int(input())
+print(pow(a,b))
+print(pow(a,b,m))
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
