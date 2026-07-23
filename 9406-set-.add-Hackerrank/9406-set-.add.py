643e39fe076019d3be52698ded8c893018a5abc6
Muhammad Irtaza <90450152+Irtaza404@users.noreply.github.com>
2026-07-23 11:47:33 +0500

    next

---
+++ 9406-set-.add-Hackerrank/9406-set-.add.py
@@ -0,0 +1,11 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+n=int(input())
+stamp=set()
+for _ in range(n):
+    stamp.add(input())
+print(len(stamp))
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
