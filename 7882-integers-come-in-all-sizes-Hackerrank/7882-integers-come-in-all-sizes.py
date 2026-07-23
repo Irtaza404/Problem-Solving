b450e110d53490532f581bc15ab1bfd64a10382e
Muhammad Irtaza <90450152+Irtaza404@users.noreply.github.com>
2026-07-23 23:39:29 +0500

    Force committed solution - Time: O(?), Space: O(?)

---
+++ 7882-integers-come-in-all-sizes-Hackerrank/7882-integers-come-in-all-sizes.py
@@ -0,0 +1,11 @@
+# Enter your code here. Read input from STDIN. Print output to STDOU
+a=int(input())
+b=int(input())
+c=int(input())
+d=int(input())
+print(a**b+c**d)
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
