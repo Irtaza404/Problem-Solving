1f4f1c5b5857f697c8dd2cec66f69ab2bdf71ab3
Muhammad Irtaza <90450152+Irtaza404@users.noreply.github.com>
2026-07-23 12:08:11 +0500

    next

---
+++ 9417-set-.union-operation-Hackerrank/9417-set-.union-operation.py
@@ -0,0 +1,11 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+n=int(input())
+a=set(map(int,input().split()))
+n=int(input())
+b=set(map(int,input().split()))
+print(len(a|b))
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
