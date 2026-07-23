f45f8054633a582294739adefb9d138713c7fe0d
Muhammad Irtaza <90450152+Irtaza404@users.noreply.github.com>
2026-07-23 22:24:50 +0500

    next

---
+++ 9951-polar-coordinates-Hackerrank/9951-polar-coordinates.py
@@ -0,0 +1,12 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import cmath
+z=complex(input())
+r=abs(z)
+p=cmath.phase(z)
+print(f"{r:.3f}")
+print(f"{p:.3f}")
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
