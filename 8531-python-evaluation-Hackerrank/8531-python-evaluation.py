d65a0cdd9a45d305e71c4dc45a2d703e52d59071
Muhammad Irtaza <90450152+Irtaza404@users.noreply.github.com>
2026-07-30 22:22:28 +0500

    Force committed solution - Time: O(n), Space: O(n)

---
+++ 8531-python-evaluation-Hackerrank/8531-python-evaluation.py
@@ -0,0 +1,7 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+eval(input())
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
