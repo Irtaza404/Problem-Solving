615eac3d4226debe65b59b64935c81bcbed9deb4
Muhammad Irtaza <90450152+Irtaza404@users.noreply.github.com>
2026-07-30 23:01:03 +0500

    Force committed solution - Time: O(n*m + n log n), Space: O(n*m)

---
+++ 8203-athlete-sort-Hackerrank/8203-athlete-sort.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    nm = input().split()
+
+    n = int(nm[0])
+
+    m = int(nm[1])
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    k = int(input())
+    arr=sorted(arr,key=lambda x :x[k])
+    for i in arr:
+        for j in i:
+            print(j,end=" ")
+        print()
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
