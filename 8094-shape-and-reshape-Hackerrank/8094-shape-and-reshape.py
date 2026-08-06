64bd1911345139816fc5fc91eff72d85c4c66301
Muhammad Irtaza <90450152+Irtaza404@users.noreply.github.com>
2026-08-06 23:40:01 +0500

    Force committed solution - Time: O(n), Space: O(n)

---
+++ 8094-shape-and-reshape-Hackerrank/8094-shape-and-reshape.py
@@ -0,0 +1,12 @@
+import numpy as np
+n=list(map(int,input().split()))
+m=np.array(n)
+m.shape=(3,3)
+print(m)
+
+
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
