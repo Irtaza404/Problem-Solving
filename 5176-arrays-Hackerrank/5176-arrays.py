273b76e4eaf043c7b879c3f803a4066baf337792
Muhammad Irtaza <90450152+Irtaza404@users.noreply.github.com>
2026-08-06 22:21:37 +0500

    Force committed solution - Time: O(n), Space: O(n)

---
+++ 5176-arrays-Hackerrank/5176-arrays.py
@@ -0,0 +1,12 @@
+
+
+def arrays(arr):
+    # complete this function
+    # use numpy.array
+    return numpy.array(list(map(float,arr))[::-1])
+
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
