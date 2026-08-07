7214300a52d608273000788fabb87fdb046eb29f
Muhammad Irtaza <90450152+Irtaza404@users.noreply.github.com>
2026-08-07 21:43:14 +0500

    Force committed solution - Time: O(N), Space: O(N)

---
+++ 13498-zeros-and-ones-Hackerrank/13498-zeros-and-ones.py
@@ -0,0 +1,11 @@
+import numpy
+a=list(map(int,input().split()))
+z=numpy.zeros((a),dtype=int)
+o=numpy.ones((a),dtype=int)
+print(z)
+print(o)
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
