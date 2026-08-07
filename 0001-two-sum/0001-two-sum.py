1112b590da4d3901b9420b5c77cc8aefd18b5dba
Muhammad Irtaza <90450152+Irtaza404@users.noreply.github.com>
2026-08-07 22:37:31 +0500

    Time: 0 ms (100%), Space: 13.2 MB (36.24%) - LeetHub - Time: O(n), Space: O(n) optimized version

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -5,11 +5,13 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
-                if nums[i]+nums[j]==target:
-                    return (i,j)
-        
+        seen={}
+        for i,n in enumerate(nums):
+            sub=target-n
+            if n in seen:
+                return [seen[n],i]
+            else:
+                seen[sub]=i
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
