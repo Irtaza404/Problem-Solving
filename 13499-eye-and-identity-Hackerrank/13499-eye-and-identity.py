15e6f93249bb7244ead4dd8fb08ab50e011ee2ea
Muhammad Irtaza <90450152+Irtaza404@users.noreply.github.com>
2026-08-07 21:49:30 +0500

    Force committed solution - Time: O(n*m), Space: O(n*m)

---
+++ 13499-eye-and-identity-Hackerrank/13499-eye-and-identity.py
@@ -0,0 +1,9 @@
+import numpy
+numpy.set_printoptions(legacy="1.13")
+n,m=map(int,input().split())
+print(numpy.eye(n,m))
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
