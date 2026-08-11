8fc4429dba04ecdd0c4970034d6a6a719d1c55fc
Muhammad Irtaza <90450152+Irtaza404@users.noreply.github.com>
2026-08-11 21:11:12 +0500

    Force committed solution - Time: O(n*m), Space: O(n+m)

---
+++ 13724-sparse-arrays-Hackerrank/13724-sparse-arrays.py
@@ -0,0 +1,57 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'matchingStrings' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. STRING_ARRAY stringList
+#  2. STRING_ARRAY queries
+#
+
+def matchingStrings(stringList, queries):
+    # Write your code here
+    res=[]
+    for q in queries:
+        t=0
+        for s in stringList:
+            if q==s:
+                t+=1
+        res.append(t)
+    return res
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    stringList_count = int(input().strip())
+
+    stringList = []
+
+    for _ in range(stringList_count):
+        stringList_item = input()
+        stringList.append(stringList_item)
+
+    queries_count = int(input().strip())
+
+    queries = []
+
+    for _ in range(queries_count):
+        queries_item = input()
+        queries.append(queries_item)
+
+    res = matchingStrings(stringList, queries)
+
+    fptr.write('\n'.join(map(str, res)))
+    fptr.write('\n')
+
+    fptr.close()
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
