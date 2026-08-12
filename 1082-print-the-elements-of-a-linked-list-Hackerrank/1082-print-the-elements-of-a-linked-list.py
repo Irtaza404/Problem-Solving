e6d1c936e5af983cec22c2bb044424ab1458dd6a
Muhammad Irtaza <90450152+Irtaza404@users.noreply.github.com>
2026-08-12 16:52:29 +0500

    Force committed solution - Time: O(n), Space: O(1)

---
+++ 1082-print-the-elements-of-a-linked-list-Hackerrank/1082-print-the-elements-of-a-linked-list.py
@@ -0,0 +1,22 @@
+
+
+# Complete the printLinkedList function below.
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+def printLinkedList(head):
+    temp=head
+    while temp!=None:
+        print(temp.data)
+        temp=temp.next
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
