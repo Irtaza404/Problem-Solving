08868079e0b6c666a43c8bac0c3347a6ddd3e3db
Muhammad Irtaza <90450152+Irtaza404@users.noreply.github.com>
2026-08-16 00:53:57 +0500

    Force committed solution - Time: O(N), Space: O(N)

---
+++ 8131-tree-preorder-traversal-Hackerrank/8131-tree-preorder-traversal.py
@@ -0,0 +1,20 @@
+
+
+"""
+Node is defined as
+self.left (the left child of the node)
+self.right (the right child of the node)
+self.info (the value of the node)
+"""
+def preOrder(root):
+    #Write your code here
+    if root!=None:
+        print(root.info,end=" ")
+        preOrder(root.left)
+        preOrder(root.right) 
+    
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
